c0b29d7c68ec50f882727ff4f2b9edd5da42a9b1
Naellu <rsdf8156@naver.com>
2023-07-04 17:12:12 +0900

    [FEAT] create project

---
+++ src/main/java/com/project/careerfair/CareerFairApplication.java
@@ -0,0 +1,13 @@
+package com.project.careerfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerFairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerFairApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/careerfair/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.project.careerfair.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String home() {
+        return "hello world";
+    }
+}
