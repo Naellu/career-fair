4c35595c64d7c5032baef39a5186c1a72483ff7e
parkchunsu <pcc0105@gmail.com>
2023-07-05 17:30:31 +0900

    [FEAT] 완전 비동기 공지사항
    완전비동기 공지사항목록
    검색기능구현완
    페이지는 인자만 넘김

---
+++ src/main/java/com/project/careerfair/controller/customer/NoticeController.java
@@ -0,0 +1,16 @@
+package com.project.careerfair.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer/notice/")
+public class NoticeController {
+
+    @GetMapping("list")
+    public String list(Model model){
+        return "customer/notice/list";
+    }
+}
