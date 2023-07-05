0506966282b4fd76a627910d3dfff08bcf977838
parkchunsu <117617050+ChunPingE@users.noreply.github.com>
2023-07-05 16:42:20 +0900

    Revert "[FEAT] 완전 비동기 공지사항 목록"

--- src/main/java/com/project/careerfair/service/customer/NoticeService.java
+++
@@ -1,9 +0,0 @@
-package com.project.careerfair.service.customer;
-
-import com.project.careerfair.domain.Notice;
-
-import java.util.List;
-
-public interface NoticeService {
-    List<Notice> getNoticeList(String search, String type);
-}
