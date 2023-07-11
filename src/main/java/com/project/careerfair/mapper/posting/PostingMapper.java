29c2ca185dd5acf1501221638152f81870b8488c
parkchunsu <pcc0105@gmail.com>
2023-07-11 16:49:57 +0900

    [update] 참여기업 상세
    참여기업 상세 정보
    참여기업 채용공고 목록
    참여기업 지난 채용공고 목록

--- src/main/java/com/project/careerfair/mapper/posting/PostingMapper.java
+++ src/main/java/com/project/careerfair/mapper/posting/PostingMapper.java
@@ -1,7 +1,15 @@
 package com.project.careerfair.mapper.posting;
 
+import com.project.careerfair.domain.Posting;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface PostingMapper {
+    List<Posting> getNowPostingList(Integer companyId);
+
+    Integer countAll(Integer companyId);
+
+    List<Posting> getPastPostingList(Integer startNum, Integer pageSize, Integer companyId);
 }
--- src/main/java/com/project/careerfair/service/company/ParticipatingCompanyServiceImpl.java
+++ src/main/java/com/project/careerfair/service/company/ParticipatingCompanyServiceImpl.java
@@ -2,13 +2,16 @@ package com.project.careerfair.service.company;
 
 import com.project.careerfair.domain.Company;
 import com.project.careerfair.domain.Industry;
+import com.project.careerfair.domain.Posting;
 import com.project.careerfair.mapper.company.CompanyMapper;
+import com.project.careerfair.mapper.posting.PostingMapper;
 import com.project.careerfair.service.admin.ExhibitionInfoService;
 import com.project.careerfair.service.industry.IndustryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +25,8 @@ public class ParticipatingCompanyServiceImpl implements ParticipatingCompanyServ
     private final IndustryService industryService;
 
     private final ExhibitionInfoService exhibitionInfoService;
+
+    private final PostingMapper postingMapper;
 
     @Override
     public Map<String, Object> getList(String search, String type, Integer industryId, Integer roundValue, Integer page) {
@@ -44,7 +49,7 @@ public class ParticipatingCompanyServiceImpl implements ParticipatingCompanyServ
     }
 
     @Override
-    public Map<String, Object> getDetail(Integer companyId) {
+    public Map<String, Object> getDetail(Integer companyId, Integer page) {
         //기업
         Company company = companyMapper.getDetail(companyId);
 
@@ -54,7 +59,48 @@ public class ParticipatingCompanyServiceImpl implements ParticipatingCompanyServ
         // 회차
         Integer round = exhibitionInfoService.getCurrentRound();
 
+        List<Posting> nowPostingList = postingMapper.getNowPostingList(companyId);
 
-        return Map.of("company", company, "industryList", industryList, "round", round);
+        Integer pageSize = 5; // 5개씩
+        Integer startNum = (page - 1) * pageSize; // 0 6 11
+
+        //페이지네이션 정보
+        //총 지난 공고 개수
+        Integer count = postingMapper.countAll(companyId);
+
+        // 마지막 페이지 번호
+        // 총 글개수 -1 / pageSize + 1
+        Integer lastPage = (count - 1) / pageSize + 1;
+
+        // 페이지네이션 왼쪽번호 1 6 11
+        Integer leftPageNum = page - 2;
+        leftPageNum = Math.max(leftPageNum, 1);
+
+        // 페이지네이션 오른쪽 번호
+        Integer rightPageNum = leftPageNum + 5;
+        rightPageNum = Math.min(rightPageNum, lastPage);
+
+        // 이전페이지
+        Integer prevPageNum = page - 1;
+
+        // 다음 페이지
+        Integer nextPageNum = page + 1;
+
+        Map<String, Object> pageInfo = new HashMap<>();
+        pageInfo.put("lastPage", lastPage);
+        pageInfo.put("leftPageNum", leftPageNum);
+        pageInfo.put("rightPageNum", rightPageNum);
+        pageInfo.put("currentPageNum", page);
+        pageInfo.put("prevPageNum", prevPageNum);
+        pageInfo.put("nextPageNum", nextPageNum);
+
+        List<Posting> pastPostingList = postingMapper.getPastPostingList(startNum, pageSize, companyId);
+
+        return Map.of("company", company,
+                "industryList", industryList,
+                "round", round,
+                "nowPostingList", nowPostingList,
+                "pageInfo", pageInfo,
+                "pastPostingList", pastPostingList);
     }
 }
