fbcf723efa18417be61d125f3ad2aded6ddba17f
parkchunsu <pcc0105@gmail.com>
2023-07-19 16:30:30 +0900

    [feat] 찾아오시는길
    찾아오시는길 추가

--- src/main/java/com/project/careerfair/controller/HomeController.java
+++ src/main/java/com/project/careerfair/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.project.careerfair.controller;
 
+import com.project.careerfair.domain.ExhibitionInfo;
+import com.project.careerfair.service.admin.ExhibitionInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeController {
+
+    @Value("${kakao.accessKey}")
+    private String accessKey;
+
+    private final ExhibitionInfoService exhibitionInfoService;
 
     @GetMapping
     public String home() {
@@ -18,5 +29,13 @@ public class HomeController {
     @GetMapping("test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("location")
+    public String location(Model model) {
+        ExhibitionInfo exhibitionInfo = exhibitionInfoService.getCurrentInfo();
+        model.addAttribute("kakaoMap", accessKey);
+        model.addAttribute("exhibitionInfo", exhibitionInfo);
+        return "exhibitionInfo/location";
     }
 }
